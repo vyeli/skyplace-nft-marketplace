a8ae6e127e679cfa8a2976cb2fbf78f1cce5dfc2
Ezequiel <eperez@itba.edu.ar>
2022-03-31 18:38:22 -0300

    chore: initial configuration

---
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/FrontController.java
@@ -0,0 +1,17 @@
+package ar.edu.itba.paw.webapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class FrontController {
+
+    @RequestMapping("/")
+    public ModelAndView home() {
+        final ModelAndView mav = new ModelAndView("frontcontroller/index");
+        return mav;
+    }
+
+}
